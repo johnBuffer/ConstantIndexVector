70b4956f6631b8b440d83d5c9ea64d465dab4526
Jean Tampon <jean.tampon@gmail.com>
2021-10-20 13:02:43 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,33 @@
 #include "index_vector.hpp"
 #include <iostream>
 
+struct Base
+{
+    int32_t base_attribute = 2;
+};
+
+struct Interface
+{
+    virtual void firstFunction() = 0;
+};
+
+struct Derived : public Base, public Interface
+{
+    float a_float = 3.0f;
+
+    void firstFunction() override
+    {
+        std::cout << "This is from derived" << std::endl;
+    }
+};
+
 
 int32_t main()
 {
-    std::vector<uint64_t> ids;
-    civ::Vector<int32_t> v;
-    
-    for (uint32_t i(0); i<10000; ++i) {
-        const uint64_t id = v.push_back(rand()%10000);
-        ids.push_back(id);
-    }
+    civ::Vector<Derived> v;
+    v.emplace_back();
 
-    for (uint32_t i(0); i<1000; ++i) {
-        // Choose an item to delete
-        const uint64_t id_i = rand()%v.size();
-        const uint64_t id = ids[id_i];
-        std::swap(ids[id_i], ids.back());
-        ids.pop_back();
-        v.erase(id);
-    }
-
-    for (uint32_t i(0); i<1000; ++i) {
-        const uint64_t id = v.push_back(rand()%10000);
-        ids.push_back(id);
-    }
-
-    for (uint64_t id : v.ids) {
-        if (id > 10000) {
-            std::cout << id << std::endl;
-        }
-    }
+    civ::PRef<Interface> 
 
     return 0;
 }
