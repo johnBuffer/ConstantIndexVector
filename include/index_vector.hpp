909f2049e894bb845a35ff07f594db35e62db0bf
Jean Tampon <jean.tampon@gmail.com>
2021-11-12 18:05:32 +0100

    Add polymorphic ref

--- include/index_vector.hpp
+++ include/index_vector.hpp
@@ -10,6 +10,8 @@ using ID = uint64_t;
 template<typename T>
 struct Ref;
 
+template<typename T>
+struct PRef;
 
 struct Slot
 {
@@ -28,6 +30,13 @@ struct ObjectSlot
 
     ID id;
     T* object;
+};
+
+
+struct GenericProvider
+{
+    virtual void* get(civ::ID id) = 0;
+    virtual bool  isValid(civ::ID, uint64_t validity_id) const = 0;
 };
 
 
@@ -52,7 +61,7 @@ struct SlotMetadata
 
 
 template<typename T>
-struct Vector
+struct Vector : public GenericProvider
 {
     Vector()
         : data_size(0)
@@ -60,28 +69,32 @@ struct Vector
     {}
     // Data ADD / REMOVE
     template<typename... Args>
-    ID emplace_back(Args&&... args);
-    ID push_back(const T& obj);
-    void erase(uint64_t id);
-    void remove_if(const std::function<bool(const T&)>& f);
+    ID                 emplace_back(Args&&... args);
+    ID                 push_back(const T& obj);
+    void               erase(ID id);
+    void               remove_if(const std::function<bool(const T&)>& f);
     // Data access by ID
-    T& operator[](ID id);
-    const T& operator[](ID id) const;
+    T&                 operator[](ID id);
+    const T&           operator[](ID id) const;
     // Returns a standalone object allowing access to the underlying data
-    Ref<T> getRef(ID id);
+    Ref<T>             getRef(ID id);
+    template<typename U>
+    PRef<U>            getPRef(ID id);
     // Returns the data at a specific place in the data vector (not an ID)
-    T& getDataAt(uint64_t i);
+    T&                 getDataAt(uint64_t i);
     // Check if the data behind the pointer is the same
-    bool isValid(ID id, ID validity) const;
+    bool               isValid(ID id, ID validity) const override;
+    uint64_t           getOperationID(ID id) const;
     // Returns the ith object and global_id
-    ObjectSlot<T> getSlotAt(uint64_t i);
+    ObjectSlot<T>      getSlotAt(uint64_t i);
     ObjectSlotConst<T> getSlotAt(uint64_t i) const;
     // Iterators
-    typename std::vector<T>::iterator begin();
-    typename std::vector<T>::iterator end();
+    typename std::vector<T>::iterator       begin();
+    typename std::vector<T>::iterator       end();
     typename std::vector<T>::const_iterator begin() const;
     typename std::vector<T>::const_iterator end() const;
-    // Number of objects in the array
+    // Number of objects in the provider
+    [[nodiscard]]
     uint64_t size() const;
 
 public:
@@ -91,24 +104,31 @@ public:
     uint64_t                  data_size;
     uint64_t                  op_count;
 
-    bool isFull() const;
-    // Returns the ID of the ith element of the data array
-    ID getID(uint64_t i) const;
+    [[nodiscard]]
+    bool          isFull() const;
+    // Returns the ID of the ith element of the data provider
+    [[nodiscard]]
+    ID            getID(uint64_t i) const;
     // Returns the data emplacement of an ID
-    uint64_t getDataID(ID id) const;
-    Slot createNewSlot();
-    Slot getFreeSlot();
-    Slot getSlot();
+    [[nodiscard]]
+    uint64_t      getDataID(ID id) const;
+    Slot          createNewSlot();
+    Slot          getFreeSlot();
+    Slot          getSlot();
     SlotMetadata& getMetadataAt(ID id);
-    const T& getAt(uint64_t id) const;
+    const T&      getAt(ID id) const;
+    [[nodiscard]]
+    void*         get(civ::ID id) override;
+
+    template<class U> friend struct PRef;
 };
 
 template<typename T>
-template<typename ...Arg>
-inline uint64_t Vector<T>::emplace_back(Arg&& ...arg)
+template<typename ...Args>
+inline uint64_t Vector<T>::emplace_back(Args&& ...args)
 {
     const Slot slot = getSlot();
-    new(&data[slot.data_id]) T(std::forward<Arg>(arg)...);
+    new(&data[slot.data_id]) T(std::forward<Args>(args)...);
     return slot.id;
 }
 
@@ -165,6 +185,12 @@ template<typename T>
 inline Ref<T> Vector<T>::getRef(ID id)
 {
     return Ref<T>(id, this, metadata[ids[id]].op_id);
+}
+
+template<typename T>
+template<typename U>
+PRef<U> Vector<T>::getPRef(ID id) {
+    return PRef<U>{id, *this, metadata[ids[id]].op_id};
 }
 
 template<typename T>
@@ -259,23 +285,34 @@ inline const T& Vector<T>::getAt(ID id) const
 }
 
 template<typename T>
-inline bool Vector<T>::isValid(ID id, uint64_t validity) const
+inline bool Vector<T>::isValid(ID id, ID validity) const
 {
     return validity == metadata[getDataID(id)].op_id;
 }
 
 template<typename T>
+inline uint64_t Vector<T>::getOperationID(ID id) const
+{
+    return metadata[getDataID(id)].op_id;
+}
+
+template<typename T>
 void Vector<T>::remove_if(const std::function<bool(const T&)>& f)
 {
-    uint64_t data_index = 0;
-    for (auto it = data.begin(); it != this->end(); ++it) {
-        if (f(*it)) {
-            this->erase(metadata[data_index].rid);
-            --it;
-        } else {
+    for (uint64_t data_index{ 0 }; data_index < data_size;) {
+        if (f(data[data_index])) {
+            erase(metadata[data_index].rid);
+        }
+        else {
             ++data_index;
         }
     }
+}
+
+template<typename T>
+void *Vector<T>::get(civ::ID id)
+{
+    return static_cast<void*>(&data[ids[id]]);
 }
 
 
@@ -324,6 +361,69 @@ private:
     ID         id;
     Vector<T>* array;
     ID         validity_id;
+};
+
+
+template<typename T>
+struct PRef
+{
+    using ProviderCallback = T*(*)(ID, GenericProvider*);
+
+    PRef()
+        : id(0)
+        , provider_callback(nullptr)
+        , provider(nullptr)
+        , validity_id(0)
+    {}
+
+    template<typename U>
+    PRef(ID index, Vector<U>& a, ID vid)
+        : id(index)
+        , provider_callback{PRef<T>::get<U>}
+        , provider(&a)
+        , validity_id(vid)
+    {}
+
+    template<typename U>
+    static T* get(ID index, GenericProvider* provider)
+    {
+        return static_cast<T*>(static_cast<U*>(provider->get(index)));
+    }
+
+    T* operator->()
+    {
+        return provider_callback(id, provider);
+    }
+
+    T& operator*()
+    {
+        return *provider_callback(id, provider);
+    }
+
+    const T& operator*() const
+    {
+        return *provider_callback(id, provider);
+    }
+
+    civ::ID getID() const
+    {
+        return id;
+    }
+
+    explicit
+    operator bool() const
+    {
+        return provider && provider->isValid(id, validity_id);
+    }
+
+private:
+    ID                  id;
+    ProviderCallback    provider_callback;
+    GenericProvider*    provider;
+    uint64_t            validity_id;
+
+    template<class U> friend struct PRef;
+    template<class U> friend struct Vector;
 };
 
 }
