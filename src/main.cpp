afbb62d7c58b57ba313e918ba1d433b659ab00da
Jean Tampon <jean.tampon@gmail.com>
2022-12-09 19:41:47 +0100

    Starting refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,17 @@
 #include "index_vector.hpp"
 #include <iostream>
 
-struct Base
-{
-    int32_t base_attribute = 2;
-};
-
-struct Interface
-{
-    virtual void firstFunction() = 0;
-};
-
-struct Derived final : public Base, public Interface
-{
-    float a_float = 3.0f;
-
-    ~Derived()
-    {
-        std::cout << "Destructor" << std::endl;
-    }
-
-    void firstFunction() override
-    {
-        std::cout << "This is from derived" << std::endl;
-    }
-};
-
 // TODO: Use pop_back() and vector API
 int32_t main()
 {
-    civ::Vector<Derived> v;
+    civ::IndexVector<int32_t> v;
+    v.push_back(0);
 
-    std::cout << "Create" << std::endl;
-    for (uint32_t i{32}; i--;) {
-        v.emplace_back();
-    }
+    std::cout << v[0] << std::endl;
+    std::cout << v.size() << std::endl;
 
-    std::cout << "Remove" << std::endl;
-
-    v.erase(15);
-
-    std::cout << "Done" << std::endl;
+    v.erase(0);
+    std::cout << v.size() << std::endl;
 
     return 0;
 }
